66c77bd4cce2393e1637786407344ae1d54a5bb2
Jum <jum@playmoolah.com>
2018-02-02 14:48:17 +0800

    Add rspec test for loading the orders request properly

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -2,14 +2,21 @@ require "rails_helper"
 
 RSpec.describe "Orders", :type => :request do
 
+  before do
+    @delivery_order = DeliveryOrder.find_or_create_by(serving_datetime: Time.zone.now.beginning_of_day + 1.day + 11.hours)
+  end
+
   it "get Orders" do
     get "/orders"
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:ok)
+
+    parsed_response = JSON.parse(response.body)
+    expect(parsed_response["orders"].is_a?(Array)).to eq(true)
+    expect(parsed_response["orders"]).to include(@delivery_order.to_custom_json.stringify_keys)
   end
 
   it "get specific Order that exists" do
-    @delivery_order = DeliveryOrder.create(serving_datetime: Time.zone.now.beginning_of_day + 1.day + 11.hours)
     get "/orders/#{@delivery_order.order_id}"
     expect(response.content_type).to eq("application/json")
     expect(response).to have_http_status(:ok)
